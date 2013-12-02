b1759e5014035f671110faa7d67ba3f70d9d5cee
Takahiro Yakoh <yakoh@sd.keio.ac.jp>
2013-12-03 05:18:19 +0900

    Modify menu.

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,61 @@
 using namespace std;
 #define UI_FILE "glade.ui"
 
+class MyImageMenuItem : public Gtk::ImageMenuItem {
+public:
+	MyImageMenuItem(BaseObjectType*, const Glib::RefPtr<Gtk::Builder>&);
+	virtual ~MyImageMenuItem();
+	int id;
+protected:
+	virtual void on_activate();
+};
+
 Gtk::Window *mainWindow;
 MyDrawingArea *drawingArea;
 Gtk::Statusbar *statusBar;
+MyImageMenuItem *menu[5];
+int state;
 
 gboolean tick(void *p){
     cout << "Tick" << endl;
     drawingArea->update();
-    return true;
+    if(state==1){
+    	return true;
+    }else{
+    	return false;
+    }
+}
+
+MyImageMenuItem::MyImageMenuItem(BaseObjectType* o, const Glib::RefPtr<Gtk::Builder>& g):
+	Gtk::ImageMenuItem(o){
+}
+MyImageMenuItem::~MyImageMenuItem(void){
+}
+void MyImageMenuItem::on_activate(void){
+	Gtk::ImageMenuItem::on_activate();
+	switch(id){
+	case 0:
+		state=1;
+		g_timeout_add(1000, tick, NULL);
+		break;
+	case 1:
+		state=0;
+		break;
+	case 2:
+		state=0;
+		break;
+	case 3:
+		state=0;
+		break;
+	case 4:
+		exit(0);
+	}
+}
+
+void on_menu_test1(void)
+{
+	cout << "Called" << endl;
+    return;
 }
 
 int main(int argc, char *argv[]){
@@ -27,8 +74,14 @@ int main(int argc, char *argv[]){
 	builder->get_widget("window1", mainWindow);
 	builder->get_widget_derived("drawingarea1", drawingArea);
 	builder->get_widget("statusbar1", statusBar);
-
-	g_timeout_add(1000, tick, NULL);
+	builder->get_widget_derived("Start", menu[0]);
+	builder->get_widget_derived("Stop", menu[1]);
+	builder->get_widget_derived("BeServer", menu[2]);
+	builder->get_widget_derived("Connect", menu[3]);
+	builder->get_widget_derived("Quit", menu[4]);
+	for(int i=0; i<5; ++i){
+		menu[i]->id=i;
+	}
 
 	kit.run(*(mainWindow));
 	return 0;
